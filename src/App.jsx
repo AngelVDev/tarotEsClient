98d991e1467a516fa8e56196276d553721f88500
Angel Scutari <antonioscutari2@hotmail.com>
2023-03-28 18:20:11 -0300

    [Try#1] Render cards in container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+// import TarotCard from "./components/TarotCard";
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      <main id='Container'>{/* <TarotCard /> */}</main>
+    </div>
+  );
 }
 
 export default App;
