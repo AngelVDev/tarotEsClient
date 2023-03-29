0dc021d9ea34ab58bfba5212143938ec2f23f4f8
Angel Scutari <antonioscutari2@hotmail.com>
2023-03-29 17:24:48 -0300

    [STYLE] Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
+import Loading from "./components/Loading";
 import Navbar from "./components/Navbar/Navbar";
-// import TarotCard from "./components/TarotCard";
+import TarotCard from "./components/TarotCard";
+import { fetchData } from "./helper/fetchData";
 
+const tarotSearch = fetchData(
+  `https://tarotapispanish.onrender.com/api/v1/cards/search?q=dios`
+);
 function App() {
+  const data = tarotSearch.read();
+
   return (
     <div className='App'>
       <Navbar />
-      <main id='Container'>{/* <TarotCard /> */}</main>
+      <main
+        id='Container'
+        className='grid grid-flow-row grid-cols-3 gap-3 justify-items-center bg-indigo-950'
+      >
+        <Suspense fallback={Loading}>
+          {data.cards.map((card) => (
+            <TarotCard
+              key={card.name_short + "carta" + card.value}
+              card={card}
+            />
+          ))}
+        </Suspense>
+      </main>
     </div>
   );
 }
