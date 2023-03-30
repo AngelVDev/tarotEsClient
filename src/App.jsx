d108550bfbd4213cd6e82524d9263898c90caaae
Angel Scutari <antonioscutari2@hotmail.com>
2023-03-30 20:03:30 -0300

    [STYLE] Best <main/>, new fetch.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from "react";
+import Footer from "./components/Footer";
 import Loading from "./components/Loading";
 import Navbar from "./components/Navbar/Navbar";
 import TarotCard from "./components/TarotCard";
 import { fetchData } from "./helper/fetchData";
 
 const tarotSearch = fetchData(
-  `https://tarotapispanish.onrender.com/api/v1/cards/search?q=dios`
+  `https://tarotapispanish.onrender.com/api/v1/random?&n=9`
 );
 function App() {
   const data = tarotSearch.read();
@@ -15,7 +16,7 @@ function App() {
       <Navbar />
       <main
         id='Container'
-        className='grid grid-flow-row grid-cols-3 gap-3 justify-items-center bg-indigo-950'
+        className='grid pb-8 grid-flow-row md:grid-cols-3 sm:grid-cols-1 gap-3 justify-items-center bg-indigo-950'
       >
         <Suspense fallback={Loading}>
           {data.cards.map((card) => (
@@ -26,6 +27,7 @@ function App() {
           ))}
         </Suspense>
       </main>
+      <Footer />
     </div>
   );
 }
