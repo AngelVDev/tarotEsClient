72e315d0e792e8f06c84e7292c19cf701c6094a1
Angel Scutari <antonioscutari2@hotmail.com>
2023-03-30 20:56:20 -0300

    [ADD] Endpoint viewer

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import React, { Suspense } from "react";
 import Footer from "./components/Footer";
 import Loading from "./components/Loading";
 import Navbar from "./components/Navbar/Navbar";
+import ShowCard from "./components/ShowCard";
 import TarotCard from "./components/TarotCard";
 import { fetchData } from "./helper/fetchData";
 
-const tarotSearch = fetchData(
-  `https://tarotapispanish.onrender.com/api/v1/random?&n=9`
-);
+const fetchUrl = `https://tarotapispanish.onrender.com/api/v1/random?&n=9`;
+
+const tarotSearch = fetchData(fetchUrl);
 function App() {
   const data = tarotSearch.read();
 
   return (
     <div className='App'>
       <Navbar />
+      <ShowCard url={fetchUrl} />
       <main
         id='Container'
         className='grid pb-8 grid-flow-row md:grid-cols-3 sm:grid-cols-1 gap-3 justify-items-center bg-indigo-950'
