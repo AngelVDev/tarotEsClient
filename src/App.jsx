2a0e093060f7f9455a6719d22015e528d7355d27
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-01 00:06:41 -0300

    [TRY #4] To make "fallback" work.

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         id='Container'
         className='grid pb-8 grid-flow-row md:grid-cols-3 sm:grid-cols-1 gap-3 justify-items-center bg-indigo-950'
       >
-        <Suspense fallback={Loading}>
+        <Suspense fallback={<Loading />}>
           {data.cards.map((card) => (
             <TarotCard
               key={card.name_short + "carta" + card.value}
