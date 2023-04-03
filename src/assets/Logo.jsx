f04f285fa9a62b814e858bb7c07a07b720dcd4d3
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-03 00:02:36 -0300

    [ADD] A hover

--- src/assets/Logo.jsx
+++ src/assets/Logo.jsx
@@ -3,7 +3,10 @@ import logo from "./logo.svg";
 
 function Logo() {
   return (
-    <div className='flex items-center justify-center flex-shrink-0 w-12 h-12 rounded-full dark:bg-violet-400'>
+    <div
+      className='flex items-center hover:bg-violet-200 
+    transition-all justify-center flex-shrink-0 w-12 h-12 rounded-full dark:bg-violet-400'
+    >
       <img
         src={logo}
         className='p-0.5'
