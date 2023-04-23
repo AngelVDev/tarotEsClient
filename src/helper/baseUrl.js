526df112e74bea81facb8d13fe46383500ce16a5
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-23 07:41:21 -0300

    [NEW] Endpoints

---
+++ src/helper/baseUrl.js
@@ -0,0 +1,5 @@
+export const baseURL = "https://tarot-api-es.vercel.app/api/v1/cards";
+export const arcanosMayoresPath = baseURL + "/type/mayor";
+export const arcanosMenoresPath = baseURL + "/type/menor";
+export const palosPath = baseURL + "/suits";
+export const cortePath = baseURL + "/courts";
