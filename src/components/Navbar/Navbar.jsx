70d8302d33118e38793deb90668dcc2d9c2a39d5
Angel Scutari <antonioscutari2@hotmail.com>
2023-05-18 12:02:37 -0300

    [MOD] Better semantic && [ADD] Reset

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,11 +8,11 @@ import { arcanosMayoresPath, arcanosMenoresPath } from "../../helper/baseUrl";
 function Navbar() {
   const { fetchData } = useStore();
   return (
-    <header
+    <nav
       className='bg-stone-100 dark:bg-gray-800 dark:text-gray-100 p-4
       '
     >
-      <div className='container flex justify-between h-16 mx-auto'>
+      <div className='container md:flex justify-between h-16 mx-auto'>
         <ul className='items-stretch hidden space-x-3 lg:flex'>
           <Corte />
           <Palos />
@@ -39,13 +39,22 @@ function Navbar() {
           rel='noopener noreferrer'
           href='/'
           aria-label='Back to homepage'
-          className='flex items-center p-2'
+          className='flex hover:bg-[#c068ff86] transition-all
+              rounded-md items-center px-4 -mb-1 border-y-2 dark:border-y-amber-400'
         >
-          Tarot API ES
+          Reiniciar
+        </a>
+        <a
+          rel='noopener noreferrer'
+          href='/'
+          aria-label='Back to homepage'
+          className='flex items-center p-3 bg-violet-900 rounded-xl text-slate-200'
+        >
+          <strong>Tarot API ES</strong>
         </a>
         <Searchbar />
       </div>
-    </header>
+    </nav>
   );
 }
 
