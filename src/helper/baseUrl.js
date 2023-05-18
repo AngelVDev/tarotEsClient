e5bc26bc2874e5d7cc07b0af176b412ac276bdfd
Angel Scutari <antonioscutari2@hotmail.com>
2023-05-18 12:02:50 -0300

    [ADD] Initial endpoint

--- src/helper/baseUrl.js
+++ src/helper/baseUrl.js
@@ -3,3 +3,4 @@ export const arcanosMayoresPath = baseURL + "/type/mayor";
 export const arcanosMenoresPath = baseURL + "/type/menor";
 export const palosPath = baseURL + "/suits";
 export const cortePath = baseURL + "/courts";
+export const randomUrl9 = "https://tarot-api-es.vercel.app/api/v1/random?&n=9";
