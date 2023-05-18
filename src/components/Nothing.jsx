ed52f75f93712de33d6cb7ab4ff488467abffbba
Angel Scutari <antonioscutari2@hotmail.com>
2023-05-18 12:00:06 -0300

    [MOD] More style?

--- src/components/Nothing.jsx
+++ src/components/Nothing.jsx
@@ -5,11 +5,15 @@ function Nothing() {
     <article
       className='w-11/12 min-h-screen text-stone-800 dark:text-neutral-50
       place-self-center flex flex-col items-center justify-center 
-     rounded-md border-cyan-200 border-2 m-auto'
+      rounded-md border-cyan-200 border-2'
     >
       <header className='text-3xl'>404</header>
       <p>No hay nada aún</p>
       <p>o no existen cartas con esa definición</p>
+      <p className='text-md text-center text-zinc-500 dark:text-neutral-600'>
+        Para volver a mostrar cartas, usa el botón <strong>[Reiniciar] </strong>
+        en la parte superior del sitio
+      </p>
       <p className='text-xl text-zinc-500 dark:text-neutral-600'>
         (elemento proximamente centrado)
       </p>
